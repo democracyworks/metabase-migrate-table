5b286779fa24e865d98be963a98bbfead00b6920
Gordon Stratton <111252+gws@users.noreply.github.com>
2026-02-23 12:51:35 -0800

    Load Metabase API key from environment

--- common.py
+++ common.py
@@ -1,14 +1,16 @@
+import os
+
 import requests
 
-METABASE_API_KEY = "REPLACE_ME"
+METABASE_API_KEY = os.getenv("METABASE_API_KEY")
 BASE_URL = "https://my_metabase_instance_url/api"
 headers = {
     "Content-Type": "application/json",
     "x-api-key": METABASE_API_KEY,
 }
 
-if METABASE_API_KEY == "change_me":
-    raise ValueError("METABASE_API_KEY needs to be updated!")
+if not METABASE_API_KEY:
+    raise ValueError("METABASE_API_KEY environment variable must be set.")
 
 
 def call_metabase_api(query_endpoint: str, method: str = "GET", data: str = ""):
